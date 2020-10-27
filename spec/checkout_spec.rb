cc97d86116e342553326e7e6c235b6af2114ac1a
dsofadog <delaney@so.fa.dog>
2020-10-27 08:07:44 +0200

    Pass:: Lavender hearts promotion

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -283,6 +283,104 @@ describe "Checkout" do
         expect(price).to eq(6678)
     end
 
+    it "lavender hearts promotion" do
+
+        # Product code  | Name                   | Price
+        # ----------------------------------------------------------
+        # 001           | Lavender heart         | £9.25
+        # 002           | Personalised cufflinks | £45.00
+        # 003           | Kids T-shirt           | £19.95        
+
+        class Catalogue
+            def initialize()
+              @items = {}
+            end
+
+            def add code:,item:
+                @items[code] = item 
+            end
+
+            def get(code:)
+              @items[code]
+            end
+
+            def count
+                @items.count
+            end
+        end
+
+        class Product
+            attr_accessor :name, :price
+            def initialize(name:,price:)
+              @name = name
+              @price = price
+            end
+        end
+
+        class Checkout
+            def initialize(rules:[],catalougue: Catalogue.new)
+                @rules = rules
+                @catalougue = catalougue
+                @scanned_items = []
+            end
+            def scan(code:)
+                @scanned_items << @catalougue.get(code: code)
+            end
+            def total
+                result = @scanned_items.map(&:price).sum
+                @rules.each do |rule|
+                    result = rule.new(items: @scanned_items).apply_rule_to result
+                end
+                result
+            end
+        end
+
+        class TenPercentPromotionalRule
+            def initialize(items:)
+              @items = items
+            end
+            def apply_rule_to total
+                if total > 6000
+                    total = total - (total*0.1)
+                end
+                total
+            end
+        end
+        
+        class LavenderHeartsPromotionalRule
+            def initialize(items:)
+              @items = items
+            end
+            def apply_rule_to total
+
+                if(@items.select{|p| p.name =="Lavender heart"}.count >=2)
+                    @items.select{|p| p.name =="Lavender heart"}.each do |product|
+                        product.price = 850
+                    end
+                end
+
+                @items.map(&:price).sum
+            end
+        end
+
+        catalogue = Catalogue.new
+        catalogue.add code: "001", item: Product.new(name: "Lavender heart", price: 925)
+        catalogue.add code: "002", item: Product.new(name: "
+            Personalised cufflinks", price: 4500)
+        catalogue.add code: "003", item: Product.new(name: "Kids T-shirt", price: 1995)
+
+        promotional_rules = []
+        promotional_rules << TenPercentPromotionalRule
+        promotional_rules << LavenderHeartsPromotionalRule
+
+        co = Checkout.new(rules:promotional_rules,catalougue: catalogue)
+        co.scan(code: "001")
+        co.scan(code: "003")
+        co.scan(code: "001")
+        price = co.total
+        expect(price).to eq(3695)
+    end
+
 
     
     
